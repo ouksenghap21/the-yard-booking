754aa12c7677417377c1e70e0242ae7804524ab0
ouksenghap21 <ouksenghap@gmail.com>
2026-05-18 21:24:21 +0700

    Update CACHE_VERSION to yard-v11-revert-desktop

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v10-trail-perf-fix";
+const CACHE_VERSION = "yard-v11-revert-desktop";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
