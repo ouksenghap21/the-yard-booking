6c062bb92797136409eddb71c9a7ce4ebc8bd446
ouksenghap21 <ouksenghap@gmail.com>
2026-05-18 23:43:11 +0700

    Update CACHE_VERSION to yard-v13-tab-bundles

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v12-nav-auth-gate";
+const CACHE_VERSION = "yard-v13-tab-bundles";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
