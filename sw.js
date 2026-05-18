2383c5c7310905b1ed4a7c7b617f0ee5a2b285bd
ouksenghap21 <ouksenghap@gmail.com>
2026-05-18 13:05:41 +0700

    Update CACHE_VERSION to v5-location-founder-faq

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v4-sponsors-premium";
+const CACHE_VERSION = "yard-v5-location-founder-faq";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
