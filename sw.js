9d1fce90bd4e742ae1a00cb55d3021ac82f2f399
ouksenghap21 <ouksenghap@gmail.com>
2026-06-23 13:05:22 +0700

    Update CACHE_VERSION to yard-v14-audit-fixes

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v13-tab-bundles";
+const CACHE_VERSION = "yard-v14-audit-fixes";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
