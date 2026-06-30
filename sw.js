4282f221aafabcd863310c6fd7add2b00f4ea494
ouksenghap21 <ouksenghap@gmail.com>
2026-07-01 02:04:32 +0700

    Update CACHE_VERSION to yard-v34-openplay-edit

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v27-safety-net";
+const CACHE_VERSION = "yard-v34-openplay-edit";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
