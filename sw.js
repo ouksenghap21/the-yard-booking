10424ab2f9bc60409ab54fbb273266f08a3715aa
ouksenghap21 <ouksenghap@gmail.com>
2026-06-30 19:22:53 +0700

    Update CACHE_VERSION to yard-v24-simplify-deals

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v19-payment-method-tracking";
+const CACHE_VERSION = "yard-v24-simplify-deals";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
