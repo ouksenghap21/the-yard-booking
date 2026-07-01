23004e906b017f50d63cf54f747163f2eef70cf4
ouksenghap21 <ouksenghap@gmail.com>
2026-07-01 12:43:37 +0700

    Update CACHE_VERSION to v35 for consolidation

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v34-openplay-edit";
+const CACHE_VERSION = "yard-v35-walkin-consolidation";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
