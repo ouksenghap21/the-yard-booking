3e7aa8bc72b19fa6982e874b7dc5ff7d5af7d41b
ouksenghap21 <ouksenghap@gmail.com>
2026-07-02 12:38:00 +0700

    Update CACHE_VERSION to v36 for booking stock receipt

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v35-walkin-consolidation";
+const CACHE_VERSION = "yard-v36-booking-stock-receipt";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
