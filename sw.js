ed0943589c33125fbd61a8856946f596b9b6e76c
ouksenghap21 <ouksenghap@gmail.com>
2026-07-02 13:18:20 +0700

    Update CACHE_VERSION to v37-wheel-restpoll

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v36-booking-stock-receipt";
+const CACHE_VERSION = "yard-v37-wheel-restpoll";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
