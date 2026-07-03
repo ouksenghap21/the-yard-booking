0075bb8e7b1ec7f964414d4bf7d59c9db013e59e
ouksenghap21 <ouksenghap@gmail.com>
2026-07-03 14:29:04 +0700

    Update CACHE_VERSION to yard-v40-refresh-btn

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v39-court-feedback";
+const CACHE_VERSION = "yard-v40-refresh-btn";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
