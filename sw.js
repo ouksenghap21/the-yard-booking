11280084be4095f0022bf94672168a5c9c4ad82c
ouksenghap21 <ouksenghap@gmail.com>
2026-07-06 00:06:21 +0700

    Update CACHE_VERSION to yard-v46-sidebar-files

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v45-corporate";
+const CACHE_VERSION = "yard-v46-sidebar-files";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
