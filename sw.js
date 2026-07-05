c95a8b12cd72cea6c0256b2f016c93a009dc2616
ouksenghap21 <ouksenghap@gmail.com>
2026-07-05 23:26:16 +0700

    Update CACHE_VERSION to yard-v45-corporate

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v43-qa-fixes";
+const CACHE_VERSION = "yard-v45-corporate";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
