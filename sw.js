6b8286f00c71bab8acf58f61959987d20c1f84d8
ouksenghap21 <ouksenghap@gmail.com>
2026-07-06 01:33:58 +0700

    Update CACHE_VERSION to yard-v47-staff-logins

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v46-sidebar-files";
+const CACHE_VERSION = "yard-v47-staff-logins";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
