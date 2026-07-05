48a44e6dbde1a6364e16baab8559d581db460a91
ouksenghap21 <ouksenghap@gmail.com>
2026-07-06 02:26:32 +0700

    Update CACHE_VERSION to yard-v49-sponsor-names

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v48-sponsors-gatefix";
+const CACHE_VERSION = "yard-v49-sponsor-names";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
