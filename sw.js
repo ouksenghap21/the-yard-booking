cd3850285c0f22defb6f62eca7828177f59d1075
ouksenghap21 <ouksenghap@gmail.com>
2026-07-11 13:31:27 +0700

    Update CACHE_VERSION to yard-v55-walkin-statelock

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v54-walkin-phone-optional";
+const CACHE_VERSION = "yard-v55-walkin-statelock";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
