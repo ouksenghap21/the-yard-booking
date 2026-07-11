dcc2a329eee328bd8e81ac32ba2ac0b6eec8d73f
ouksenghap21 <ouksenghap@gmail.com>
2026-07-11 10:42:46 +0700

    Update CACHE_VERSION to v54-walkin-phone-optional

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v53-coach-hardened";
+const CACHE_VERSION = "yard-v54-walkin-phone-optional";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
