1c013fba178a5b4a76a824cadc51f61627a86c60
ouksenghap21 <ouksenghap@gmail.com>
2026-07-12 23:52:10 +0700

    Update CACHE_VERSION to yard-v62-markpaid-report

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v60-extend-booking";
+const CACHE_VERSION = "yard-v62-markpaid-report";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
