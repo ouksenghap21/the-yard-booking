785ea3aa8efd6de481f553ded33679dd6f9b4861
ouksenghap21 <ouksenghap@gmail.com>
2026-07-13 00:49:39 +0700

    Update CACHE_VERSION to v63-roster-cta

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v62-markpaid-report";
+const CACHE_VERSION = "yard-v63-roster-cta";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
