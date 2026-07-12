a923d1399b1041f34ee05ce0c29e69e5bc66cfcf
ouksenghap21 <ouksenghap@gmail.com>
2026-07-12 16:47:35 +0700

    Update CACHE_VERSION to yard-v60-extend-booking

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v59-coach-court-fix";
+const CACHE_VERSION = "yard-v60-extend-booking";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
