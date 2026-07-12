ee68716b974b3e460639105b43288706d3e276ad
ouksenghap21 <ouksenghap@gmail.com>
2026-07-12 14:51:59 +0700

    Update CACHE_VERSION to v57-coaching-attach

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v56-member-discount";
+const CACHE_VERSION = "yard-v57-coaching-attach";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
