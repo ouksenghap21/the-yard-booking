fbcb82af250b7f666fbcdbc4e4f316ba43516cd3
ouksenghap21 <ouksenghap@gmail.com>
2026-07-13 20:33:41 +0700

    Update CACHE_VERSION to yard-v67-money-clarity

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v66-courts-textfix";
+const CACHE_VERSION = "yard-v67-money-clarity";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
