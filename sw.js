ef94e14649be6ea451155efab801cc998fa20533
ouksenghap21 <ouksenghap@gmail.com>
2026-07-14 00:02:38 +0700

    Update CACHE_VERSION to version 68

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v67-money-clarity";
+const CACHE_VERSION = "yard-v68-partial-release-selfedit";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
