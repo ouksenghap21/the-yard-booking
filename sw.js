108bcbba4046dae6d15835e145ec30b73a673bdc
ouksenghap21 <ouksenghap@gmail.com>
2026-07-14 08:41:26 +0700

    Update CACHE_VERSION to yard-v69-noshow-writeoff-editcourts

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v68-partial-release-selfedit";
+const CACHE_VERSION = "yard-v69-noshow-writeoff-editcourts";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
