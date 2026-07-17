557a4541b9552886c4e1b4b66f39be8d50ee9169
ouksenghap21 <ouksenghap@gmail.com>
2026-07-17 13:44:31 +0700

    Update CACHE_VERSION to yard-v73-waive-hardened

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v70-usersweep";
+const CACHE_VERSION = "yard-v73-waive-hardened";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
