412114a5f3a14b6b846ede58b2898047427f8205
ouksenghap21 <ouksenghap@gmail.com>
2026-07-18 16:05:10 +0700

    Update CACHE_VERSION to yard-v79-cancel-midsession

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v77-multiadd";
+const CACHE_VERSION = "yard-v79-cancel-midsession";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
