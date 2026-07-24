15860f60cd649d3e2fefa2739d0e32030a03ca2d
ouksenghap21 <ouksenghap@gmail.com>
2026-07-25 02:18:01 +0700

    Update CACHE_VERSION to v86-tournament-line

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v85-delta-tracer";
+const CACHE_VERSION = "yard-v86-tournament-line";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
