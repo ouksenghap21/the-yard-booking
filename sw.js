0a72127caf1b66ac19c15003e7f51ee267d2d55f
ouksenghap21 <ouksenghap@gmail.com>
2026-07-24 23:52:03 +0700

    Update CACHE_VERSION to yard-v85-delta-tracer

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v84-validator-epoch";
+const CACHE_VERSION = "yard-v85-delta-tracer";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
