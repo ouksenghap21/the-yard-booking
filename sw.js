fcd1beb1d38c2f6a93c0153a80241c16fff6e73f
ouksenghap21 <ouksenghap@gmail.com>
2026-07-24 12:38:10 +0700

    Update CACHE_VERSION for data validation

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v79-cancel-midsession";
+const CACHE_VERSION = "yard-v81-datavalidation";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
