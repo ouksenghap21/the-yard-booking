0a03127c2ed90d0460e618a626f5eabeef7748da
ouksenghap21 <ouksenghap@gmail.com>
2026-07-24 17:36:11 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v81-datavalidation";
+const CACHE_VERSION = "yard-v83-autofix";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
