7b9b72e563481ab34884bffae3b24b162aba3040
ouksenghap21 <ouksenghap@gmail.com>
2026-07-27 10:05:58 +0700

    Update CACHE_VERSION to yard-v88-unified-spend

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v87-customer-spend";
+const CACHE_VERSION = "yard-v88-unified-spend";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
