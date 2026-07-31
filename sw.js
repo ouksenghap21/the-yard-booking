9636211ea35a5ad27e070c0019f1c2bd5b4b080a
ouksenghap21 <ouksenghap@gmail.com>
2026-07-31 10:57:11 +0700

    Update cache version to yard-v96-release-fix

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v95-walkin-move";
+const CACHE_VERSION = "yard-v96-release-fix";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
