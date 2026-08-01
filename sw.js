be00c37ceae5a7c6e3946b406ec3de68420987e7
ouksenghap21 <ouksenghap@gmail.com>
2026-08-01 21:02:12 +0700

    Update CACHE_VERSION to yard-v102-reprice

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v101-stocktake";
+const CACHE_VERSION = "yard-v102-reprice";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
