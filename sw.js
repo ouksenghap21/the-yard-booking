aa0f604debfb56a52bc56d6d6ce71ac0c0749f2e
ouksenghap21 <ouksenghap@gmail.com>
2026-08-03 23:54:48 +0700

    Update CACHE_VERSION to yard-v118-coachmath

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v112-fairuse-page";
+const CACHE_VERSION = "yard-v118-coachmath";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
