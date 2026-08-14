c10af8fc3a2b8dd89065a6e40e284c54d27c8752
ouksenghap21 <ouksenghap@gmail.com>
2026-08-14 07:36:52 +0700

    Update CACHE_VERSION to yard-v139-consignmodel

--- sw.js
+++ sw.js
@@ -46,7 +46,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v138-datefix";
+const CACHE_VERSION = "yard-v139-consignmodel";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
