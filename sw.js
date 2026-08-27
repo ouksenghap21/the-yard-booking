74844377a07f13d69b589d02803dc7e9b9495434
ouksenghap21 <ouksenghap@gmail.com>
2026-08-27 10:42:07 +0700

    Update CACHE_VERSION to yard-v147-validatorfix

--- sw.js
+++ sw.js
@@ -46,7 +46,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v146-counterfix";
+const CACHE_VERSION = "yard-v147-validatorfix";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
