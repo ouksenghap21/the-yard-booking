8b2e917134485f39f5e42adf5805d297a2e03e1c
ouksenghap21 <ouksenghap@gmail.com>
2026-08-28 13:03:13 +0700

    Update CACHE_VERSION to yard-v151-tabtruth

--- sw.js
+++ sw.js
@@ -46,7 +46,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v147-validatorfix";
+const CACHE_VERSION = "yard-v151-tabtruth";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
