d582293da442550442088923046888ca5d69e1e1
ouksenghap21 <ouksenghap@gmail.com>
2026-09-22 14:41:32 +0700

    Update CACHE_VERSION in service worker

--- sw.js
+++ sw.js
@@ -46,7 +46,7 @@
  */
 "use strict";
 
-const CACHE_VERSION = "yard-v175-kpitodate";
+const CACHE_VERSION = "yard-v182-intent";
 const VENDOR_CACHE = "yard-vendor-v1";
 
 // Vendor URLs we know are immutable (include a version in the path)
